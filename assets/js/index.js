4668406831cd647ebc17e074109d326350f9007c
Ciarra Murphy <mciarra@ymail.com>
2018-02-19 17:47:11 -0500

    added first questions to js file

---
+++ assets/js/index.js
@@ -0,0 +1,13 @@
+const questionsAnswers = [
+    {
+        question: "When was the Magna Carta written",
+        answers: [1492, 1215, 1776],
+        correct: 1215
+    } , {
+        question: "Which monarch was beheaded during the English Civil War",
+        answers: ["Charles I", "John", "Edward II"],
+        correct: "Charles I"
+    } , {
+        
+    }
+];

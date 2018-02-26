80ec930aaf214d29d740921fd25eb969ce7f5986
Ciarra Murphy <mciarra@ymail.com>
2018-02-26 16:18:02 -0500

    added remaining questions and first css

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,13 +1,20 @@
 const questionsAnswers = [
     {
-        question: "When was the Magna Carta written",
+        question: "When was the Magna Carta written?",
         answers: [1492, 1215, 1776],
         correct: 1215
     } , {
-        question: "Which monarch was beheaded during the English Civil War",
+        question: "Which monarch was beheaded during the English Civil War?",
         answers: ["Charles I", "John", "Edward II"],
         correct: "Charles I"
     } , {
-        
+        question: "Who was the monarch during the American Revolution?",
+        answers: ["George I", "George II", "George III"],
+        correct: "George III"
+    } , {
+        question: "Which monarch was known as the 'Virgin Queen'?",
+        answers: ["Mary I", "Elizabeth I", "Victoria"],
+        correct: "Elizabeth I"
     }
 ];
+

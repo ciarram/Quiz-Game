7ba95e2b9195a9d48d4a2188bb118b76ad81501f
Ciarra Murphy <mciarra@ymail.com>
2018-02-28 17:36:57 -0500

    reformatted question array and setting up the game in one large object

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,26 @@
 $(document).ready(function(){
 //section containting the questions and answers
+const answers = $('.answers');
+
+const questionsAnswers = [
+    {
+        question: "When was the Magna Carta written?",
+        answers: [1492, 1215, 1776],
+        correct: 1215
+    },  {
+        question: "Which monarch was beheaded during the English Civil War?",
+        answers: ["Charles I", "John", "Edward II"],
+        correct: "Charles I"
+    } , {
+        question: "Who was the monarch during the American Revolution?",
+        answers: ["George I", "George II", "George III"],
+        correct: "George III"
+    } , {
+        question: "Which monarch was known as the 'Virgin Queen'?",
+        answers: ["Mary I", "Elizabeth I", "Victoria"],
+        correct: "Elizabeth I"
+    }
+];
 const questionOne = [
     {
         question: "When was the Magna Carta written?",
@@ -7,27 +28,27 @@ const questionOne = [
         correct: 1215
     }
 ] ;
-const questionTwo = [
-    {
-        question: "Which monarch was beheaded during the English Civil War?",
-        answers: ["Charles I", "John", "Edward II"],
-        correct: "Charles I"
-    } 
-];
-const questionThree = [ 
-    {
-        question: "Who was the monarch during the American Revolution?",
-        answers: ["George I", "George II", "George III"],
-        correct: "George III"
-    }
-];
-const questionFour = [
-    {
-        question: "Which monarch was known as the 'Virgin Queen'?",
-        answers: ["Mary I", "Elizabeth I", "Victoria"],
-        correct: "Elizabeth I"
-    }
-];
+// const questionTwo = [
+//     {
+//         question: "Which monarch was beheaded during the English Civil War?",
+//         answers: ["Charles I", "John", "Edward II"],
+//         correct: "Charles I"
+//     } 
+// ];
+// const questionThree = [ 
+//     {
+//         question: "Who was the monarch during the American Revolution?",
+//         answers: ["George I", "George II", "George III"],
+//         correct: "George III"
+//     }
+// ];
+// const questionFour = [
+//     {
+//         question: "Which monarch was known as the 'Virgin Queen'?",
+//         answers: ["Mary I", "Elizabeth I", "Victoria"],
+//         correct: "Elizabeth I"
+//     }
+// ];
 //The page is set up to hide most of the buttons except for start
 
 $('.questions').hide();
@@ -45,50 +66,40 @@ $('#start').on('click', function(){
     $('#first-answer').show();
     $('#second-answer').show();
     $('#third-answer').show();
-    firstQuestion();
+    $('h1').hide();
+    //firstQuestion();
+    quiz.questions();
 });
 
-const firstQuestion = () => {
-    questionOne.forEach(function(element) {
-    console.log(element);
-    $('#question').append(element.question);
-    $('#first-answer').append(element.answers[0]);
-    $('#second-answer').append(element.correct);
-    $('#third-answer').append(element.answers[2]);
-    $('.buttons').on('click', function(){
-        if(element.correct){
-            console.log("Incorrect");
-        } else {
-            console.log("Correct ");
-        }
-    })
-   });
+
+// const firstQuestion = (questions) => {
+//     questionOne.forEach(function(questions) {
+//         console.log(questions);
+//         $('#question').append(questions.question);
+//         $('#first-answer').append(questions.answers[0]);
+//         $('#second-answer').append(questions.answers[1]);
+//         $('#third-answer').append(questions.answers[2]);
+//     });
+// };
+
+var quiz = {
+    questionsAnswers: questionsAnswers,
+    currentQuestion: 0,
+    right: 0,
+    wrong: 0,
+
+    questions: function(){
+        $('#question').append(questionsAnswers[this.currentQuestion].question);
+        
+        // for(let i = 0; questionsAnswers[this.currentQuestion].answers.length; i++){   
+        //     answers.append("<button class = 'answer-btn' data-name='" + questionsAnswers[this.currentQuestion].answers[i]
+        //     + "'>" + questionsAnswers[this.currentQuestion].answers[i] + "</button>")
+        // }
+        console.log("I'm in the question function!");
+    }
+
 };
 
-   questionTwo.forEach(function(element) {
-
-    console.log(element);
-});
-
-questionThree.forEach(function(element) {
-
-    console.log(element);
-});
-
-questionFour.forEach(function(element) {
-
-    console.log(element);
-});
-
-// const firstQuestion = () => {
-//    $('#question').append(questionsAnswers[0].question);
-
-
-    // $('#first-answer').append(questionsAnswers[0].answers[0]);
-    // $('#second-answer').append(questionsAnswers[0].correct);
-    // $('#third-answer').append(questionsAnswers[0].answers[2]);
-    // console.log("Function " + questionsAnswers[0].question);
-//}
 
 
 });
